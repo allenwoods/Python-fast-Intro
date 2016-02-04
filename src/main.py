46a924b6dc9270c126d34dd556ba657436f9da6d
Allen Woods <allen.woods@foxmail.com>
2016-02-04 15:14:31 +0800

    Update Basic

---
+++ src/main.py
@@ -0,0 +1,13 @@
+
+if __name__ == "__main__":
+    # name = input("What's your name:")
+    # greeting = "Hello,%s"%name
+    # l_count = greeting.count('l')
+    # l_percent = (l_count/len(greeting))*100
+    # print(greeting)
+    # print("The length of the sentence is %d, there are %d 'l' in it(%.2f%%)."
+    #       %(len(greeting),l_count,l_percent))
+    a=259
+    b=259
+    print(a==b)
+    print(a is b)
